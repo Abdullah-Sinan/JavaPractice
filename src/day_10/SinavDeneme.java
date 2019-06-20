8752ca878668291d7fbdc9b4e47348900c4c1010
Sinan Oduncu <a.sinanoduncu@gmail.com>
2019-06-20 19:20:22 -0400

    second commit

--- src/day_10/SinavDeneme.java
+++ src/day_10/SinavDeneme.java
@@ -2,21 +2,21 @@ package day_10;
 
 public class SinavDeneme {
 
-	public static boolean isSorted(int[] a) 
-	{
-	    int i;
-	    for(i = 0; i < a.length; i ++);{
-	        if (a[i] < a[i+1]) {
-	            return true;
-	        } else {
-	            return false;   
-	        }
-	    }
+	public static boolean isSorted(int[] a) {
+		int i;
+		for (i = 0; i < a.length; i++){
+			if (a[i] < a[i + 1]) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return false;
 	}
-	public static void main(String[] args)
-	{
-	    int ar[] = {3,5,6,7};
-	    System.out.println(isSorted(ar));   
+
+	public static void main(String[] args) {
+		int ar[] = { 3, 5, 6, 7 };
+		System.out.println(isSorted(ar));
 	}
 
 }
